0c2885258df27724b712b654e0f704e7798c3f2f
user.name <kravchenko.web@gmail.com>
2020-01-20 11:25:14 +0300

    Project initiation

---
+++ Practice/Section1/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    new Vue({
+        el: "#app",
+        data: {
+            helloWorld: "Hello 1 world!"
+        }
+    });
+});
