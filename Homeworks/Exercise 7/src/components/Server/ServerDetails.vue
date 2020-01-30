bea0deb039a2c511164974a99d87fe8f609e268d
user.name <kravchenko.web@gmail.com>
2020-01-30 15:01:23 +0300

    Section 8 completed

---
+++ Homeworks/Exercise 7/src/components/Server/ServerDetails.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <p v-if="!server">Please select server</p>
+        <p v-else>#{{server.id}} status: {{server.status}}  <button @click="normalize()">Change status</button></p>
+        
+    </div>
+
+</template>
+
+<script>
+    import {serverBus} from '../../main';
+
+    export default {
+        data: function() {
+            return {
+                server: null
+            }
+        },
+        methods: {
+            normalize() {
+                this.server.status = 'Normal';
+            }
+        },
+        created() {
+            serverBus.$on('selectedServer', (server) => {
+                this.server = server;
+            });
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Homeworks/Exercise 7/src/components/Server/Servers.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="col-xs-12 col-sm-6">
+        <ul class="list-group">
+            <listItem :key="server" v-for="server in servers" :server="server"></listItem>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import listItem from './listItem.vue';
+
+    export default {
+        components: {
+            'listItem': listItem
+        },
+        data: function() {
+            return {
+                servers: [
+                    {
+                        id: 1,
+                        status: 'Critical'
+                    },
+                    {
+                        id: 2,
+                        status: 'Normal'
+                    },
+                    {
+                        id: 3,
+                        status: 'Unknown'
+                    },
+                    {
+                        id: 4,
+                        status: 'Critical'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
