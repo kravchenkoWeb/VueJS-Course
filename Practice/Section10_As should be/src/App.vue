45a7e28f308ec16eea7d11bf2c8787737aa0c332
user.name <kravchenko.web@gmail.com>
2020-02-02 14:04:16 +0300

    Added teachers version of practicing project

---
+++ Practice/Section10_As should be/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxQuotes="maxQuotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quote-grid @quoteDeleted="deleteQuote" :quotes="quotes"></app-quote-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Infor: Click on box to delete it
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import QuotesGrid from './components/QuotesGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+    import Header from './components/Header.vue';
+    export default {
+        data: function() {
+            return {
+                maxQuotes: 10,
+                quotes: ['Just quote to see']
+            }
+        },
+        components: {
+            appQuoteGrid: QuotesGrid,
+            appNewQuote: NewQuote,
+            appHeader: Header 
+        },
+        methods: {
+            newQuote(quote) {
+                if(this.quotes.length >= this.maxQuotes) {
+                    alert("Please delete quotes first");
+                }
+                this.quotes.push(quote);
+            },
+            deleteQuote(index) {
+                this.quotes.splice(index, 1);
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
