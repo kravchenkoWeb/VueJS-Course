2262e3dea3ac6c25c1583352feebbf86b4693407
user.name <kravchenko.web@gmail.com>
2020-02-20 13:59:43 +0300

    Section 16 started

---
+++ Practice/Section16/src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+
+    <ul class="nav">
+            <router-link to="/" active-class="active" exact><a>Home</a></router-link>
+            <router-link to="/user/10" active-class="active"><a>User</a></router-link>
+        </ul>
+</template>
---
+++ Practice/Section16/src/components/user/User.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>The User Page</h1>
+        <hr>
+        <button class="btn btn-primary" @click="navigateHome">Go home</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    methods: {
+        navigateHome() {
+            this.$router.push('/');
+        }
+    }
+}
+</script>
