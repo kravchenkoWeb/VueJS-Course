6b4ab94e9eda9a3b3d9c032ad6e72f764883d927
user.name <kravchenko.web@gmail.com>
2020-03-06 18:21:07 +0200

    Section 17 completed

---
+++ Practice/Section17/src/components/Counter.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <button class="btn btn-primary" @click="asyncIncrement({by: 100, duration: 2000})">Increment</button>
+        <button class="btn btn-primary" @click="asyncDecrement({by: 100, duration: 2000})">Decrement</button>
+    </div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    export default {
+        methods: {
+            ...mapActions([
+                'asyncIncrement',
+                'asyncDecrement',
+            ])
+        }
+    }
+</script>
